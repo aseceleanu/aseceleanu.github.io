90e58a7e888cb5a86e12c4ccc0d6c286c563cc11
Alexandra Seceleanu <alexandra.seceleanu@gmail.com>
2023-12-26 13:01:51 -0600

    upload publication bib

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -56,7 +56,7 @@ def html_escape(text):
 
 for pubsource in publist:
     parser = bibtex.Parser()
-    bibdata = parser.parse_file(publist[pubsource]["file"])
+    bibdata = parser.parse_file(publist[pubsource]["/publications/AS.bib"])
 
     #loop through the individual references in a given bibtex file
     for bib_id in bibdata.entries:
